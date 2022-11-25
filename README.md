5dd5bd25384bdbc11eaf69563f6936297d5d29cf
muthurajajava <115981581+muthurajajava@users.noreply.github.com>
2022-11-25 08:52:34 -1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecom-final-fakeshop-api
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gv2r9p)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
+import Product from './components/Product';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/product/:id" element={<Product />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
